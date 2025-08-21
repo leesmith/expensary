3b6d101680074ceb4856f99b79981c8bbe2c5218
Lee Smith <jeremyleesmith@gmail.com>
2025-08-21 08:23:49 -0500

    Add tailwindcss forms plugin

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,8 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@tailwindplus/elements", to: "@tailwindplus--elements.js" # @1.0.9
+pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.10
+pin "mini-svg-data-uri" # @1.4.4
+pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @4.1.12
+pin "tailwindcss/defaultTheme", to: "tailwindcss--defaultTheme.js" # @4.1.12
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.12
