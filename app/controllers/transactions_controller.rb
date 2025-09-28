b6a2a7b3463b92a86d371f0b8ba32f84ed39777a
Lee Smith <jeremyleesmith@gmail.com>
2025-09-28 09:04:14 -0500

    Add pagination

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,7 @@
 class TransactionsController < ApplicationController
   def index
     @transactions = Transaction.includes(:account).order(tran_date: :desc)
+    @pagy, @transactions = pagy(@transactions, limit: 50)
   end
 
   def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  include Pagy::Frontend
+
   def word_date(date)
     date.blank? ? nil : date.in_time_zone("Central Time (US & Canada)").strftime("%B %d, %Y")
   end
