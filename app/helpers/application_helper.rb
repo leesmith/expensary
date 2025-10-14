0900a96e6da130e8d9d602bd8a4fe38ad40318d1
Lee Smith <jeremyleesmith@gmail.com>
2025-10-13 21:45:18 -0500

    Use optgroup for category select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
   def word_date(date)
     date.blank? ? nil : date.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")
   end
+
+  def grouped_category_options(categories, selected_category_id)
+    optgroup = ""
+    categories.map(&:group_title).uniq.each do |group|
+      optgroup << "<optgroup label=\"#{group}\">"
+      categories.select { |c| c.group_title == group }.each do |category|
+        if selected_category_id.present? && category.id == selected_category_id
+          optgroup << "<option value=\"#{category.id}\" selected=\"selected\">#{category.title}</option>"
+        else
+          optgroup << "<option value=\"#{category.id}\">#{category.title}</option>"
+        end
+      end
+      optgroup << "</optgroup>"
+    end
+    optgroup
+  end
 end
