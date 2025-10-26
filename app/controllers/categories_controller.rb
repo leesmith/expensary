380e6e1148aed37682b7a24849deec37f89e8d6d
Lee Smith <jeremyleesmith@gmail.com>
2025-10-26 15:17:34 -0500

    Category editing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,16 +6,25 @@ class CategoriesController < ApplicationController
   def create
     category = Category.new(category_params)
     if category.save
-      redirect_to categories_url, success: "The category was successfully added."
+      redirect_to categories_url, success: "The category was successfully added!"
     else
       render :index, status: :unprocessable_content
     end
   end
 
-  def edit
+  def update
+    category = Category.find params.expect(:id)
+    if category.update(category_params)
+      redirect_to categories_url, success: "The category was successfully updated!"
+    else
+      redirect_to categories_url, error: "The category could not be saved!"
+    end
   end
 
-  def update
+  def destroy
+    category = Category.find params.expect(:id)
+    category.destroy
+    redirect_to categories_url, success: "The category was successfully deleted!"
   end
 
   private
