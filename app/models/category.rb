9a25076501faae98ceadf379a285022e990365e1
Lee Smith <jeremyleesmith@gmail.com>
2025-10-26 22:42:22 -0500

    Calculate totals on dashboard

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :transactions, dependent: :nullify
+
+  scope :income, -> { where(group_title: "Income") }
+  scope :expense, -> { where.not(group_title: "Income") }
+
 end
