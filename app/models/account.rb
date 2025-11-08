522727fea23ee8ac001f6c676c47a8f50829072d
Lee Smith <jeremyleesmith@gmail.com>
2025-11-07 21:33:05 -0600

    Show latest transaction date

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,8 @@ class Account < ApplicationRecord
   validates :name, presence: true
 
   has_many :transactions, dependent: :destroy
+
+  def latest_transaction
+    self.transactions.order(tran_date: :desc).first
+  end
 end
