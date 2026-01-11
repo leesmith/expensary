eba2696277924e4491155d83e92a58f4b4f717da
Lee Smith <jeremyleesmith@gmail.com>
2026-01-11 14:31:37 -0600

    Bundle update and importmap update

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,12 +5,13 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@tailwindplus/elements", to: "@tailwindplus--elements.js" # @1.0.9
-pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.10
+pin "@tailwindplus/elements", to: "@tailwindplus--elements.js" # @1.0.21
+pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.11
 pin "mini-svg-data-uri" # @1.4.4
-pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @4.1.12
-pin "tailwindcss/defaultTheme", to: "tailwindcss--defaultTheme.js" # @4.1.12
-pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.12
+pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @4.1.18
+pin "tailwindcss/defaultTheme", to: "tailwindcss--defaultTheme.js" # @4.1.18
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.18
 pin "el-transition" # @0.0.7
 pin "echarts", to: "echarts.min.js"
 pin "echarts/theme/dark", to: "echarts/theme/dark.js"
+pin "tailwindcss" # @4.1.18
---
+++ vendor/javascript/tailwindcss.js
@@ -0,0 +1,4 @@
+// tailwindcss@4.1.18 downloaded from https://ga.jspm.io/npm:tailwindcss@4.1.18/dist/lib.mjs
+
+export{K as Features,R as Polyfills,n as __unstable__loadDesignSystem,r as compile,Z as compileAst,H as default}from"../_/DWRZpizZ.js";import"../_/CSLAA2v3.js";import"../_/Cay67bh1.js";
+
